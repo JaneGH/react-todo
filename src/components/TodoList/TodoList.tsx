d7fbf34d8b288017466cd9b563e15842b723dfa0
JaneGH <jkrytsyna@gmail.com>
2025-01-27 19:51:35 -0800

    added pages

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./TodoList.module.css";
 import TodoListItem from "../TodoListItem/TodoListItem";
 import { TodoListProps } from "../../utils/types";
@@ -8,17 +8,61 @@ const TodoList: React.FC<TodoListProps> = ({
   onRemoveTodo,
   onToggleComplete,
 }) => {
+  const itemsPerPage = 7;
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(todoList.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const currentItems = todoList.slice(startIndex, startIndex + itemsPerPage);
+
+  const handlePageChange = (page: number) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
   return (
-    <ul className={styles.todoList}>
-      {todoList.map((todo) => (
-        <TodoListItem
-          key={todo.id}
-          todo={todo}
-          onRemoveTodo={onRemoveTodo}
-          onToggleComplete={onToggleComplete}
-        />
-      ))}
-    </ul>
+    <div>
+      <ul className={styles.todoList}>
+        {currentItems.map((todo) => (
+          <TodoListItem
+            key={todo.id}
+            todo={todo}
+            onRemoveTodo={onRemoveTodo}
+            onToggleComplete={onToggleComplete}
+          />
+        ))}
+      </ul>
+
+      <div className={styles.pagination}>
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          className={`${styles.pageButton} ${styles.prevButton}`}
+        >
+          &lt;
+        </button>
+
+        {[...Array(totalPages)].map((_, index) => (
+          <button
+            key={index}
+            onClick={() => handlePageChange(index + 1)}
+            className={`${styles.pageButton} ${
+              currentPage === index + 1 ? styles.activePage : ""
+            }`}
+          >
+            {index + 1}
+          </button>
+        ))}
+
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className={`${styles.pageButton} ${styles.nextButton}`}
+        >
+          &gt;
+        </button>
+      </div>
+    </div>
   );
 };
 
